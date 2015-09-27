d83d8bb717ed0e47ecbcee9b41d6954f917d0fe3
Max <hoeselm@sec.in.tum.de>
2015-09-27 18:11:02 +0200

    v 0.1 Initial Commit - Clean Start

--- app/src/main/java/thane/hhgttg/Manual.java
+++ app/src/main/java/thane/hhgttg/Manual.java
@@ -2,10 +2,8 @@ package thane.hhgttg;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class Manual extends AppCompatActivity {
 
@@ -13,8 +11,6 @@ public class Manual extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
-        TextView tv = (TextView) findViewById(R.id.textView7);
-        tv.setMovementMethod(new ScrollingMovementMethod());
     }
 
     @Override
